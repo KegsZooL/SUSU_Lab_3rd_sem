6294ad4e24fe6c1c38aa76e059c60cc152a96f15
KegsZooL <kegs.zool.2015@inbox.ru>
2023-09-08 23:29:45 +0300

    update lab 2

--- lab2/IntegerSet.cs
+++ lab2/IntegerSet.cs
@@ -5,7 +5,7 @@ namespace lab_2
 {
     class IntegerSet
     {
-        public HashSet<int> _HashSet { get; private set; }
+        public HashSet<int> _HashSet { get; set; }
 
         public IntegerSet() => _HashSet = new HashSet<int>();
 
@@ -16,20 +16,49 @@ namespace lab_2
             IntegerSet unionHashSet = new IntegerSet(set_1._HashSet.Concat(set_2._HashSet));
             return unionHashSet;
         } 
-        private static IntegerSet Union(IntegerSet set_1, int value)
+
+        private static IntegerSet Union(IntegerSet set, int value)
         {
-            set_1._HashSet.Add(value);
+            set._HashSet.Add(value);
+            return set;
+        }
+
+        private static IntegerSet Remove(IntegerSet set, int value) 
+        {
+            set._HashSet.Remove(value);
+            return set;
+        }
+
+        private static IntegerSet Remove(IntegerSet set_1, IntegerSet set_2) 
+        {
+            set_1._HashSet.ExceptWith(set_2._HashSet);
             return set_1;
         }
 
-        public static IntegerSet operator +(IntegerSet set_1, IntegerSet set_2) 
-        {
+        //private static bool Compare(IntegerSet set_1, IntegerSet set_2) 
+        //{
+        //    return true;
+        //}
+
+        public static IntegerSet operator +(IntegerSet set_1, IntegerSet set_2){
             return Union(set_1, set_2);
         }
         
-        public static IntegerSet operator +(IntegerSet set_1, int value) 
-        {
-            return Union(set_1, value);
+        public static IntegerSet operator +(IntegerSet set, int value) {
+            return Union(set, value);
         }
+
+        public static IntegerSet operator -(IntegerSet set, int value) {
+            return Remove(set, value);
+        }
+
+        public static IntegerSet operator -(IntegerSet set_1, IntegerSet set_2) {
+            return Remove(set_1, set_2);
+        }
+
+        //public static IntegerSet operator <=(IntegerSet set_1, IntegerSet set_2) {
+        //    return Compare(set_1, set_2);
+        //}
+
     }
 }
--- lab2/OperationHandler.cs
+++ lab2/OperationHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Sharprompt;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace lab_2
 {
@@ -14,6 +15,9 @@ namespace lab_2
                 {"2",  new IntegerSet(new List<int> { 3, 2, 7, 4, 1 }) }
             };
 
+            string definiteSet; 
+            IEnumerable<int> value;
+
             var typeOfOperation = Prompt.Select("Выберите тип операции", new[] {"[1] Добавить элемент в множество.", 
                 "[2] Удалить элемент в множестве.", "[3] Объединить множества.", "[4] Сравнить множества.", "[5] Разность множеств."});
 
@@ -21,18 +25,35 @@ namespace lab_2
             {
                 case ('1'):
 
-                    var definiteSet = Prompt.Select("В какое множество вы хотите добавить элемент?", dict.Keys);
-                    var value = Prompt.List<int>($"Введите число, которое хотите добавить в множество №{definiteSet}");
+                    definiteSet = Prompt.Select("В какое множество вы хотите добавить элемент?", dict.Keys);
+                    value = Prompt.List<int>($"Введите число, которое хотите добавить в множество №{definiteSet}");
 
                     dict[definiteSet] = dict[definiteSet] + value.ToArray<int>()[0];
+
+                    Console.WriteLine("\n==================Добавление элемента прошло успешно!==================\n");
 
                     break;
 
                 case ('2'):
 
+                    definiteSet = Prompt.Select("В каком множестве вы хотите удалить элемент?", dict.Keys);
+                    value = Prompt.List<int>($"Введите число, которе хотите удалить в множестве №{definiteSet}");
+
+                    int previousLength = dict[definiteSet]._HashSet.Count;
+
+                    dict[definiteSet] = dict[definiteSet] - value.ToArray<int>()[0];
+
+                    if (previousLength == dict[definiteSet]._HashSet.Count)
+                        Console.WriteLine($"\n==================Элемент {value.ToArray<int>()[0]} не находится в множестве!==================\n");
+                    else
+                        Console.WriteLine($"\n==================Удаление элемента прошло успешно!==================\n");
+
                     break;
 
                 case ('3'):
+                    dict["1"] = dict["1"] + dict["2"];
+
+                    Console.WriteLine($"\n==================Объединение множеств прошло успешно!==================\n");
 
                     break;
 
@@ -41,6 +62,15 @@ namespace lab_2
                     break;
 
                 case ('5'):
+
+                    definiteSet = Prompt.Select("Какое множество будет уменьшаемым?", dict.Keys);
+
+                    if (definiteSet == "1")
+                        dict["1"] = dict["1"] - dict["2"];
+                    else
+                        dict["2"] = dict["2"] - dict["1"];
+
+                    Console.WriteLine($"\n==================Разность множеств прошла успешно!==================\n");
 
                     break;
             }
