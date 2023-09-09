973e3414e25d97249e42e0ab073e2ec3f43b25e9
KegsZooL <kegs.zool.2015@inbox.ru>
2023-09-09 21:41:22 +0300

    new lab 3

---
+++ lab3/Figure.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace lab3
+{
+    abstract class Figure
+    {   
+        public double FrameThickness { get; }
+        public double Radius { get; }
+        public double Length { get; }
+        public double Height { get; }
+
+        public Figure(double _frameThickness, double _radius) 
+        {
+            FrameThickness = _frameThickness;
+            Radius = _radius;
+        }        
+        
+        public Figure(double _frameThickness, double _length, double _height) 
+        {
+            FrameThickness = _frameThickness;
+            Length = _length;
+            Height = _height;
+        }        
+            
+        public abstract double GetSquare();
+    }
+
+    class Circle : Figure
+    {   
+        public Circle(double _frameThickness, double _radius) : base(_frameThickness, _radius){ }
+
+        public override double GetSquare() => Math.PI * (Math.Pow(Radius + FrameThickness, 2) - Math.Pow(Radius, 2));
+    }
+
+    class Square : Figure
+    {
+        public Square(double _frameThickness, double _length, double _height) : base(_frameThickness, _length, _height) { }
+
+        public override double GetSquare() => Math.Pow(Length + (2 * FrameThickness), 2) - Math.Pow(Length, 2);
+    }
+    
+    class Ellipse : Figure
+    {
+        public Ellipse(double _frameThickness, double _length, double _height) : base(_frameThickness, _length, _height) { }
+
+        public override double GetSquare() => Math.PI * (Length + FrameThickness) * (Height + FrameThickness) - Math.PI * Length * Height;
+    }
+    
+    class Rectangle : Figure
+    {
+        public Rectangle(double _frameThickness, double _length, double _height) : base(_frameThickness, _length, _height) { }
+
+        public override double GetSquare() => (Length + 2 * FrameThickness) * (Height + 2 * FrameThickness) - (Length * Height);
+    }
+}
---
+++ lab3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using lab3; // временно
+
+namespace Program
+{
+    class Program
+    {
+        static void Main() 
+        {
+
+        }
+    }
+}
